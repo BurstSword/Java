fe4456d162f1406f063d98b6633ba3ec505ce4e5
MiguelER <miguelespigaresr@gmail.com>
2018-10-23 09:56:20 +0200

    Final

--- Boletín2/src/Ejercicio1.java
+++ Boletín2/src/Ejercicio1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int orden;
+        int mayor;
         System.out.print("Ingrese 3 números ");
         int num1 = teclado.nextInt();
         int num2 = teclado.nextInt();
@@ -11,21 +11,21 @@ public class Ejercicio1 {
 
 
         if (num1 > num2) {
-            orden = num1;
+            mayor = num1;
             num1 = num2;
-            num2 = orden;
+            num2 = mayor;
         }
 
         if (num2 > num3) {
-            orden = num2;
+            mayor = num2;
             num2 = num3;
-            num3 = orden;
+            num3 = mayor;
         }
 
         if (num1 > num2) {
-            orden = num1;
+            mayor = num1;
             num1 = num2;
-            num2 = orden;
+            num2 = mayor;
         }
 
         System.out.println("los números ordenados son " + num1 + " " + num2 + " y " + num3);
