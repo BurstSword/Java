6d6452b7f11f5a0f81b7829738c486cd3f02e4ec
MiguelER <miguelespigaresr@gmail.com>
2018-10-25 22:29:41 +0200

    Ni idea por qué hay cambios si no he tocado nada

--- Boletín3/src/Ejercicio6.java
+++ Boletín3/src/Ejercicio6.java
@@ -7,51 +7,49 @@ public class Ejercicio6 {
         int menor, mayor;
 
 
-
         System.out.println("Ingrese 3 números y los ordenadoremos de menor a mayor y de mayor a menor");
         for (int i = 0; i < orden.length; i++) {
             orden[i] = teclado.nextInt();
         }
 
-            if (orden[0] > orden[1]) {
-                menor = orden[0];
-                orden[0] = orden[1];
-                orden[1] = menor;
-            }
+        if (orden[0] > orden[1]) {
+            menor = orden[0];
+            orden[0] = orden[1];
+            orden[1] = menor;
+        }
 
-            if (orden[1] > orden[2]) {
+        if (orden[1] > orden[2]) {
+            menor = orden[1];
+            orden[1] = orden[2];
+            orden[2] = menor;
+        }
 
-                menor = orden[1];
-                orden[1] = orden[2];
-                orden[2] = menor;
-            }
-
-            if (orden[0] > orden[1]) {
-                menor = orden[0];
-                orden[0] = orden[1];
-                orden[1] = menor;
-            }
+        if (orden[0] > orden[1]) {
+            menor = orden[0];
+            orden[0] = orden[1];
+            orden[1] = menor;
+        }
 
         System.out.println("los números ordenados de menor a mayor son: " + orden[0] + " " + orden[1] + " y " + orden[2]);
 
-            if (orden[0] < orden[1]) {
-                mayor = orden[0];
-                orden[0] = orden[1];
-                orden[1] = mayor;
-            }
+        if (orden[0] < orden[1]) {
+            mayor = orden[0];
+            orden[0] = orden[1];
+            orden[1] = mayor;
+        }
 
-            if (orden[1] < orden[2]) {
+        if (orden[1] < orden[2]) {
 
-                mayor = orden[1];
-                orden[1] = orden[2];
-                orden[2] = mayor;
-            }
+            mayor = orden[1];
+            orden[1] = orden[2];
+            orden[2] = mayor;
+        }
 
-            if (orden[0] < orden[1]) {
-                mayor = orden[0];
-                orden[0] = orden[1];
-                orden[1] = mayor;
-            }
+        if (orden[0] < orden[1]) {
+            mayor = orden[0];
+            orden[0] = orden[1];
+            orden[1] = mayor;
+        }
 
         System.out.println("los números ordenados de mayor a menor son: " + orden[0] + " " + orden[1] + " y " + orden[2]);
     }
