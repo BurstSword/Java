95577263f946b41bb37de6fc86c554fd479c982c
MiguelER <miguelespigaresr@gmail.com>
2018-11-07 14:19:05 +0100

    ghghg

---
+++ Boletín4/src/boletin4/Ejercicio1.java
@@ -0,0 +1,24 @@
+package boletin4;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Ejercicio1 ej = new Ejercicio1();
+
+        ej.leerNum();
+        ej.suma();
+    }
+    int leerNum(){
+        int num1;
+        Scanner teclado = new Scanner(System.in);
+        num1=teclado.nextInt();
+        return num1;
+    }
+    int suma(int num1){
+        int suma;
+        suma=num1;
+        return suma;
+    }
+    void
+}
