5d03f996b638633e3a72aab9c1d80f0e3a1375e0
MiguelER <miguelespigaresr@gmail.com>
2018-11-08 09:54:05 +0100

    fff

--- Boletín4/src/boletin4/Ejercicio5Funciones.java
+++ Boletín4/src/boletin4/Ejercicio5Funciones.java
@@ -6,7 +6,7 @@ public class Ejercicio5Funciones {
     public static int pedirDimensionArray() {
         int dimensionArray;
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingrese la cantidad de número que quiera ingresar");
+        System.out.println("Ingrese la cantidad de números que quiera ingresar");
         dimensionArray = teclado.nextInt();
         return dimensionArray;
     }
@@ -22,14 +22,16 @@ public class Ejercicio5Funciones {
         }
         return numeros;
     }
-    public static int sumarValores(int[] numeros){
-        int sumatorio=0;
-        for(int i=0; i<numeros.length;i++){
-            sumatorio=sumatorio+numeros[i];
+
+    public static int sumarValores(int[] numeros) {
+        int sumatorio = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            sumatorio = sumatorio + numeros[i];
         }
         return sumatorio;
     }
-    public static void mostrarResultado(int sumatorio){
+
+    public static void mostrarResultado(int sumatorio) {
         System.out.println("La suma de los valores ingresados es: " + sumatorio);
     }
 
