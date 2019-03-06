5a39205353c09fc5b2d5f1fcc283b462ee0565e9
Miguel <miguelespigares@gmail.com>
2019-03-06 10:17:39 +0100

    gr

--- PruebaGit/PruebaGit/src/Principal.java
+++ PruebaGit/PruebaGit/src/Principal.java
@@ -2,7 +2,7 @@
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Vamos a operarrferrrr");
+        System.out.println("Vamos a operar q");
     }
 
 }
