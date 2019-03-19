0717a26d044bbb91b1900f2d3768f397c894df18
Miguel <miguelespigares@gmail.com>
2019-03-19 09:16:39 +0100

    gg

---
+++ NuevoProyectoPOO/src/Gestoras/GestoraMenu.java
@@ -0,0 +1,11 @@
+package Gestoras;
+
+
+public class GestoraMenu {
+    public static void menuPrincipal(){
+        System.out.println("Bienvenido");
+        System.out.println("1. Crear personajes");
+        System.out.println("2. Gestoras.GestoraDado");
+        System.out.println("Bienvenido");
+    }
+}
