080b863cdb9a0ae4b137e6577418dfc2ee2317de
Miguel <miguelespigares@gmail.com>
2019-03-27 13:32:10 +0100

    ghg

---
+++ GUI/src/PrimerEjercicio/HolaMundo.java
@@ -0,0 +1,27 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.Color.*;
+
+public class HolaMundo extends JFrame {
+
+    public HolaMundo() throws HeadlessException {
+        super("Hola, mundo");
+        JLabel label = new JLabel("Hola, mundo", JLabel.CENTER);
+        getContentPane().add(label);
+        getContentPane().setBackground(black);
+        setSize(200,100);
+        setVisible(true);
+        label.setForeground(white);
+        label.setFont(new Font("Verdana", Font.BOLD, 24));
+    }
+
+
+
+
+
+
+
+}
