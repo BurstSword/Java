1e80262777403720a1d483cd9fc08a7a0b2e9eb5
Miguel <miguelespigares@gmail.com>
2019-04-01 11:57:06 +0200

    hf

--- GUI/src/PrimerEjercicio/HolaMundo.java
+++ GUI/src/PrimerEjercicio/HolaMundo.java
@@ -9,11 +9,11 @@ public class HolaMundo extends JFrame {
 
     public HolaMundo() throws HeadlessException {
         super("Hola, mundo");
+        setSize(300,100);
+        setVisible(true);
         JLabel label = new JLabel("Hola, mundo", JLabel.CENTER);
         getContentPane().add(label);
         getContentPane().setBackground(black);
-        setSize(200,100);
-        setVisible(true);
         label.setForeground(white);
         label.setFont(new Font("Verdana", Font.BOLD, 24));
     }
