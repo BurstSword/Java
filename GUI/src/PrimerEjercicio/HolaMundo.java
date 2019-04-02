3c119ecab47d98678c57ed5b7579185e82705e4c
Miguel <miguelespigaresr@gmail.com>
2019-04-02 07:48:36 +0200

    bf

--- GUI/src/PrimerEjercicio/HolaMundo.java
+++ GUI/src/PrimerEjercicio/HolaMundo.java
@@ -16,5 +16,6 @@ public class HolaMundo extends JFrame {
         getContentPane().setBackground(black);
         label.setForeground(white);
         label.setFont(new Font("Verdana", Font.BOLD, 24));
+
     }
 }
