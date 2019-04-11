6e777b2b6fa2a9e4b1de6cc45e6505cd73702d80
Miguel <miguelespigaresr@gmail.com>
2019-04-11 23:45:39 +0200

    dffsd

--- BoletinGUI2/src/Ejercicio9/Ejercicio9GUI.java
+++ BoletinGUI2/src/Ejercicio9/Ejercicio9GUI.java
@@ -7,20 +7,27 @@ import java.awt.event.ActionListener;
 
 public class Ejercicio9GUI extends JFrame implements ActionListener {
     private JButton convertir;
+    private JLabel texto1, texto2;
     private JTextField ptas, euros;
     private JPanel panel;
     private GridLayout gridLayout;
+    private GridLayout gridLayoutTextos;
 
     public Ejercicio9GUI() throws HeadlessException {
         super("Ejercicio 9");
         this.convertir = new JButton("Convertir");
         this.ptas = new JTextField(10);
         this.euros = new JTextField(10);
+        this.texto1 = new JLabel("Pesetas", JLabel.CENTER);
+        this.texto2 = new JLabel("Euros", JLabel.CENTER);
         this.panel = new JPanel();
+        this.gridLayout = new GridLayout(2, 1);
+        this.gridLayoutTextos = new GridLayout(2, 2);
+        panel.setLayout(gridLayoutTextos);
+        panel.add(texto1);
+        panel.add(texto2);
         panel.add(ptas);
         panel.add(euros);
-        this.gridLayout = new GridLayout(2, 2);
-
         getContentPane().setLayout(gridLayout);
         getContentPane().add(panel);
         getContentPane().add(convertir);
@@ -30,16 +37,13 @@ public class Ejercicio9GUI extends JFrame implements ActionListener {
 
 
     public String convertirAEuros(String ptasStr) {
+        final double europtas = 166.386;
         double euros;
         double ptas;
         String eurosStr;
-        double europtas = 166.386;
         ptas = Double.parseDouble(ptasStr);
-
         euros = ptas / europtas;
-
         eurosStr = Double.toString(euros);
-
 
         return eurosStr;
     }
--- BoletinGUI2/src/Ejercicio9/MainEj9.java
+++ BoletinGUI2/src/Ejercicio9/MainEj9.java
@@ -4,6 +4,6 @@ public class MainEj9 {
     public static void main(String[] args) {
         Ejercicio9GUI ejercicio9GUI = new Ejercicio9GUI();
         ejercicio9GUI.setVisible(true);
-        ejercicio9GUI.setSize(150,200);
+        ejercicio9GUI.setSize(300,200);
     }
 }
