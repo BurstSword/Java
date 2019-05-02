d7a140794b5e9b5f5b440ae31073b842d9f840fa
Miguel <miguelespigaresr@gmail.com>
2019-05-02 07:32:05 +0200

    mhh

---
+++ CountdownTimer/JS/Countdown.js
@@ -0,0 +1,46 @@
+window.onload=initialize;
+var timeinterval;
+
+function initialize(){
+document.getElementById("btnRestart").addEventListener("click",restartClock,false);
+initializeClock();
+
+}
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+
+  return {
+    'total': t,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock() {
+  var deadline = new Date(Date.parse(new Date()) + 30 * 60 * 1000);
+  var clock = document.getElementById("clockdiv");
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(deadline);
+
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+  updateClock();
+  timeinterval=setInterval(updateClock, 1000)
+}
+
+function restartClock(){
+  clearInterval(timeinterval);
+  initializeClock();
+}
+
+
