42d3b657fd81a043c74fb8b5861818638627c567
Miguel <miguelespigaresr@gmail.com>
2019-05-11 12:47:52 +0200

    fdfd

--- CountdownTimer/JS/Countdown.js
+++ CountdownTimer/JS/Countdown.js
@@ -1,11 +1,13 @@
 window.onload = initialize;
 
-var timeinterval;
-var timeRemaining;
-var btnStop;
-var btnPlay;
-var btnPause;
-var btnResume;
+var timeinterval; //El intervalo de tiempo que se recorre
+var timeRemaining; //El tiempo restante que queda por recorrer
+var btnStop; //Botón que reiniciar el reloj y lo pone en 30 minutos
+var btnPlay;//Botón que comienza la cuenta atrás
+var btnPause//Botón que pausa la cuenta atrás en el punto en el que está
+var btnResum//Botón que que la reanuda
+
+//Método que inicializa todos los botones y los eventos de cada uno de ellos
 function initialize() {
   btnStop = document.getElementById("btnStop");
   btnPlay = document.getElementById("btnPlay");
@@ -23,6 +25,7 @@ function initialize() {
   stopClock();
 }
 
+//Método que se encarga de comprobar el tiempo restante que queda y lo devuelve
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
@@ -35,17 +38,20 @@ function getTimeRemaining(endtime) {
   };
 }
 
+//Método que inicializa el reloj con el tiempo que le damos en la variable timeRemaining
 function initializeClock() {
   var deadline = new Date(Date.parse(new Date()) + timeRemaining);
   var clock = document.getElementById("clockdiv");
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
+  //Método que actualiza el reloj segundo a segundo
   function updateClock() {
     var t = getTimeRemaining(deadline);
 
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    //Le vamos restando 1000 milisegundos que son un segundo
     timeRemaining -= 1000;
     if (t.total <= 0) {
       clearInterval(timeinterval);
@@ -56,6 +62,7 @@ function initializeClock() {
   timeinterval = setInterval(updateClock, 1000)
 }
 
+//Reinicia el reloj a 30
 function restartClock() {
   clearInterval(timeinterval);
   timeRemaining = 1800000;
@@ -63,15 +70,17 @@ function restartClock() {
   stopClock();
 }
 
+//Pausa el reloj
 function stopClock() {
   clearInterval(timeinterval);
-
 }
 
+//Reanuda el reloj
 function resumeClock() {
   initializeClock();
-
 }
+
+//Métodos que cambian los botones unos por otros
 
 function changeStop() {
   btnStop.style.display = 'none';
